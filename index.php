626c73e4588b93aa08e0fde62df46ad451a2d695
Profile-exe <kang67572346@gmail.com>
2021-11-11 22:25:21 +0900

    로그인을 위한 세션 추가 및 index.php 디자인 수정

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/classes/db.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/classes/pagination.class.php';
+
+// 세션 시작
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// 메시지 전달 시 알림
+if (isset($_GET['msg'])) {
+    echo '<script>alert("'.$_GET['msg'].'");</script>';
+}
+
+$loginout = '<a id="loginout_btn" href="/manage_member/login.php" class="col-6 mb-3 btn btn-secondary">로그인</a>';
+if (isset($_SESSION['user_id']) && $_SESSION['user_id'] != '') {
+    $loginout = '<a id="loginout_btn" href="/manage_member/process_logout.php" class="col-6 mb-3 btn btn-secondary">로그아웃</a>';
+}
+?>
+
 <!doctype html>
 <html lang="ko">
 <head>
@@ -15,18 +35,18 @@
 <main class="container">
     <article class="my-5 row d-flex justify-content-center">
         <section class="row mb-5 d-flex justify-content-center">
-            <header class="col-6">
-                <form class="input-group form-floating d-flex justify-content-center" action="/manage_bus/search_bus.php">
-                    <div class="col-10 form-floating">
+            <header class="col-7">
+                <form class="input-group form-floating d-flex justify-content-center w-100" action="/manage_bus/search_bus.php">
+                    <div id="input-line-num" class="form-floating w-75">
                         <input type="text" class="form-control" id="floatingInput" placeholder="노선 이름 입력">
-                        <label for="floatingInput">노선 이름 입력</label>
+                        <label for="floatingInput">정류소 이름 입력</label>
                     </div>
-                    <button class="btn btn-outline-primary" type="submit">검색</button>
+                    <button id="search-btn" class="btn btn-outline-primary" type="submit">검색</button>
                 </form>
             </header>
         </section>
-        <section class="row mb-5 d-flex justify-content-evenly">
-            <div class="col-md-6 card" style="width: 18rem;">
+        <section class="row mb-5 d-flex justify-content-center">
+            <div class="col-md-6 mx-4 card" style="width: 18rem;">
                 <a href="/manage_bus/favorites.php" class="text-decoration-none">
                     <img class="mt-3 card-img-top" src="/img/favorite.png" alt="...">
                     <div class="card-body">
@@ -34,7 +54,7 @@
                     </div>
                 </a>
             </div>
-            <div class="col-md-6 card" style="width: 18rem;">
+            <div class="col-md-6 mx-4 card" style="width: 18rem;">
                 <a href="/manage_bus/search_bus.php" class="text-decoration-none">
                     <img class="mt-3 card-img-top" src="/img/bus-stop.png" alt="...">
                     <div class="card-body">
@@ -43,12 +63,15 @@
                 </a>
             </div>
         </section>
-        <section class="row">
+        <section class="row d-flex justify-content-center">
             <div class="row d-flex justify-content-center">
-                <button id="fetch_bus_info" class="col-7 mb-3 btn btn-secondary">버스 정보 업데이트</button>
+                <?=$loginout?>
             </div>
             <div class="row d-flex justify-content-center">
-                <button id="fetch_station_info" class="col-7 mb-3 btn btn-secondary">정류소 정보 업데이트</button>
+                <button id="fetch_bus_info" class="col-6 mb-3 btn btn-outline-success">버스 정보 업데이트</button>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <button id="fetch_station_info" class="col-6 mb-3 btn btn-outline-success">정류소 정보 업데이트</button>
             </div>
         </section>
     </article>
